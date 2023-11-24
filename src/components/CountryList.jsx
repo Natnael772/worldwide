c0e992c0b276b7ff56eeb1533166c3e950f7cfc0
natnael772 <natnaeldeyas0@gmail.com>
2023-11-24 11:12:03 +0300

    feat: display message if no city is added

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -8,6 +8,11 @@ import CountryItem from "./CountryItem";
 export default function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
   const countries = [];
 
   return (
