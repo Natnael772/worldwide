8054cbcb9c4911700f208022fd028d33164507d0
natnael772 <natnaeldeyas0@gmail.com>
2023-11-24 10:17:53 +0300

    feat: add citylist component to display list of cities

---
+++ src/components/CityList.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import styles from "./CityList.module.css";
+
+export default function CityList() {
+  return (
+    <ul className={styles.cityList}>
+      <p>city</p>
+    </ul>
+  );
+}
