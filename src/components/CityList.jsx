3667753d7911cb410f68dc1e7070f51065bc1c9a
natnael772 <natnaeldeyas0@gmail.com>
2023-11-24 10:20:43 +0300

    feat: display message to add city

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -7,6 +7,11 @@ import Message from "./Message";
 export default function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
