c26e31f9c3c0c9063d13d54c534c2ae8d3e87116
natnael772 <natnaeldeyas0@gmail.com>
2023-11-24 11:10:51 +0300

    feat: display spinner for loading state

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import styles from "./CountryList.module.css";
-
+import Spinner from "./Spinner";
+import CityItem from "./CityItem";
+import Message from "./Message";
 import CountryItem from "./CountryItem";
 
 export default function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
   const countries = [];
 
   return (
